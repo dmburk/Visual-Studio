3ae49063b95594cf6bbca40d085907b6f065e329
Michael Burk <michael.burk@gmail.com>
2011-08-11 15:44:30 -0400

    Added happybirthday project.

---
+++ HappyBirthday/HappyBirthday/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            HappyBirthday birthdayMessage;
+            birthdayMessage = new HappyBirthday();
+            // You can do the above in one line.
+            // HappyBirthday birthdayMessage = new HappyBirthday();
+
+            // MessageBox.Show(birthdayMessage.getMessage("Mike"));
+
+            birthdayMessage.MyProperty = "Mike";
+            birthdayMessage.PresentCount = 5;
+
+            string returnedMessage;
+            returnedMessage = birthdayMessage.MyProperty;
+            MessageBox.Show(returnedMessage);
+        }
+    }
+}
---
+++ HappyBirthday/HappyBirthday/HappyBirthday.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+    class HappyBirthday
+    {
+        public HappyBirthday()
+        {
+            numberOfPresents = 0;
+        }
+
+        private string getMessage(string givenName)
+        {
+            string theMessage;
+
+            theMessage = "Happy Birthday " + givenName + "\n";
+            theMessage += "Number of presents =" + numberOfPresents.ToString();
+
+            return theMessage;
+        }
+
+        private string birthdayMessage;
+        public string MyProperty 
+        { 
+            get { return birthdayMessage; }
+            set { birthdayMessage = getMessage(value); }
+        }
+
+        private int numberOfPresents;
+
+        public int PresentCount
+        {
+            set { numberOfPresents = value; }
+        }
+    }
+}
